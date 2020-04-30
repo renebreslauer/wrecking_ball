2ce6fdde00a80d4500ea385c5c200206ef79279d
renebreslauer <renebreslauer@me.com>
2020-04-30 11:05:44 -0400

    wrecking ball

--- src/components/Menu/MenuButton.jsx
+++ src/components/Menu/MenuButton.jsx
@@ -1,32 +1,12 @@
 import React from 'react'
 import '../../App.scss'
-import Lottie from 'react-lottie'
-import Initials from '../../assets/initials.svg'
-import animationData from '../../lotties/pinkblob.json'
-import YellowBlob from '../../lotties/yellowblob.json'
+import Logo from '../../assets/logo.svg'
 
 class MenuButton extends React.Component {
   render() {
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice',
-      },
-    }
-
     return (
       <button id="MenuButton" onMouseDown={this.props.handleMouseDown}>
-        <div className="animation_overlay">
-          <img src={Initials} className="initials_svg" />
-        </div>
-        <Lottie
-          className="blobanimation"
-          options={defaultOptions}
-          height={300}
-          width={300}
-        />
+        <img src={Logo} className="logo_svg" />
       </button>
     )
   }
